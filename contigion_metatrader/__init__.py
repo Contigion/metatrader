e6287512332f0ef7d4fcbf2682cd8afda480edfa
Datumza <reitumetsemasilo@gmail.com>
2025-01-03 14:40:07 +0200

    feat: Added helper functions for opening/closing trades

--- contigion_metatrader/__init__.py
+++ contigion_metatrader/__init__.py
@@ -1,3 +1,8 @@
-__all__ = ["mt_connect"]
+__all__ = ["mt_connect", "mt_history"]
 
 from mt_connect import connect, disconnect
+from mt_history import (get_trade_history_by_hours_ago, get_trade_history_by_days_ago, get_trade_history_by_year,
+                        get_trade_close_history_by_hours_ago, get_trade_close_history_by_days_ago,
+                        get_trade_close_history_by_year, get_profit_loss_history, get_profit_loss_history_totals,
+                        get_profit_loss_history_count
+                        )
