c2d9821dc606f23765eb11bb2a63e5286503f313
Datumza <reitumetsemasilo@gmail.com>
2025-01-03 14:59:55 +0200

    feat: Added account info retrieval functions

--- contigion_metatrader/__init__.py
+++ contigion_metatrader/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ["mt_connect", "mt_history", "mt_trades"]
+__all__ = ["mt_connect", "mt_history", "mt_trades", "mt_account"]
 
 from mt_connect import connect, disconnect
 from mt_history import (get_trade_history_by_hours_ago, get_trade_history_by_days_ago, get_trade_history_by_year,
@@ -6,3 +6,5 @@ from mt_history import (get_trade_history_by_hours_ago, get_trade_history_by_day
                         get_trade_close_history_by_year, get_profit_loss_history, get_profit_loss_history_totals,
                         get_profit_loss_history_count
                         )
+from .mt_account import (get_account_balance, get_account_credit_balance, get_account_number, get_account_name,
+                         get_net_profit, get_open_net_profit, get_free_margin, get_used_margin, get_account_exposure)
