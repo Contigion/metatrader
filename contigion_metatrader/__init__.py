447ae133ccfa14b0bcecb97b6d58240a7bea9d24
Datumza <reitumetsemasilo@gmail.com>
2025-01-03 15:10:34 +0200

    feat: Added functions for opening/closing trades

--- contigion_metatrader/__init__.py
+++ contigion_metatrader/__init__.py
@@ -1,5 +1,6 @@
-__all__ = ["mt_connect", "mt_history", "mt_trades", "mt_account"]
+__all__ = ["mt_connect", "mt_history", "mt_trades", "mt_account", "mt_actions"]
 
+from . import util
 from mt_connect import connect, disconnect
 from mt_history import (get_trade_history_by_hours_ago, get_trade_history_by_days_ago, get_trade_history_by_year,
                         get_trade_close_history_by_hours_ago, get_trade_close_history_by_days_ago,
@@ -8,3 +9,5 @@ from mt_history import (get_trade_history_by_hours_ago, get_trade_history_by_day
                         )
 from .mt_account import (get_account_balance, get_account_credit_balance, get_account_number, get_account_name,
                          get_net_profit, get_open_net_profit, get_free_margin, get_used_margin, get_account_exposure)
+from .mt_actions import (open_long_trade, open_short_trade, close_trade, adjust_take_profit, adjust_stop_loss,
+                         adjust_stops)
